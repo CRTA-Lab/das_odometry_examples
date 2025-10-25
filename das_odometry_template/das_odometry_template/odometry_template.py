4af0418f4d8e7fde4cead2b6143ad499c380a11b
BCaran <branimir.caran@gmail.com>
2025-10-25 10:36:00 +0200

    minor changes

--- das_odometry_template/das_odometry_template/odometry_template.py
+++ das_odometry_template/das_odometry_template/odometry_template.py
@@ -1,5 +1,34 @@
-def main():
-    print('Hi from das_odometry_template.')
+import rclpy
+from rclpy.node import Node
+
+from sensor_msgs.msg import JointState
+from nav_msgs.msg import Odometry
+
+class JointStateToOdom(Node):
+
+    def __init__(self):
+        super().__init__('jointstate_to_odom')
+
+        # Subscriber
+        self.sub_ = self.create_subscription(
+            JointState, '/joint_states', self.joint_state_callback, 10)
+
+        # Publisher
+        self.pub_ = self.create_publisher(Odometry, '/das/odometry', 10)
+
+    def joint_state_callback(self, msg: JointState):
+        # Blank callback for now
+        # You can process `msg` and publish Odometry here
+        odom_msg = Odometry()
+        self.pub_.publish(odom_msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = JointStateToOdom()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
